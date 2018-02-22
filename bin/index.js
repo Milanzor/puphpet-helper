a988148cc18880e436c48ea44c844b9c9966a169
Milan van As <milan@hetwonen.nl>
2018-02-22 08:21:26 +0100

    Added string template, added a few questions, added yaml template and read it, cool stuff

--- bin/index.js
+++ bin/index.js
@@ -2,38 +2,82 @@
 
 const fs = require('fs');
 const path = require('path');
-
-const argv = require('minimist')(process.argv.slice(2));
-const isElevated = require('is-elevated');
-const hostile = require('hostile');
+const randomstring = require('randomstring');
+const format = require('string-template');
 
 const inquirer = require('inquirer');
 
-isElevated().then(function (isElevated) {
-    if (isElevated) {
-        console.log(`Running elevated, showing contents of host file`);
-        hostile.get(false, (err, lines) => {
-            if (err) {
-                console.error(err.message);
-            }
-            lines.forEach(function (line) {
-                console.log(line);
-            })
-        });
-    }
-});
+const templatePath = path.resolve(__dirname, '..', 'templates');
 
-inquirer.prompt([
+// const isElevated = require('is-elevated');
+// const hostile = require('hostile');
+//
+// isElevated().then(function (isElevated) {
+//     if (isElevated) {
+//         console.log(`Running elevated, showing contents of host file`);
+//         hostile.get(false, (err, lines) => {
+//             if (err) {
+//                 console.error(err.message);
+//             }
+//             lines.forEach(function (line) {
+//                 console.log(line);
+//             });
+//         });
+//     }
+// });
+
+console.log(`Hi, let's do some work.`);
+
+const questions = [
     {
-        type: 'list',
-        name: 'size',
-        message: 'What size do you need?',
-        choices: ['Large', 'Medium', 'Small'],
+        type: 'input',
+        name: 'vhostservername',
+        message: 'Domain?',
+        default: 'example.local',
+        filter: function (val) {
+            return val.toLowerCase();
+        }
+    },
+    {
+        type: 'input',
+        name: 'vhostdocroot',
+        message: 'Document root?',
+        default: '/var/www/example.local/public_html',
+        filter: function (val) {
+            return val.toLowerCase();
+        }
+    },
+    {
+        type: 'input',
+        name: 'vhostport',
+        message: 'Port?',
+        default: '80',
         filter: function (val) {
             return val.toLowerCase();
         }
     }
-]).then(answers => {
-    console.log(answers);
-}).catch(function () {
+];
+
+inquirer.prompt(questions).then(answers => {
+
+    // Read the template of the vhost
+    const vhost = fs.readFileSync(path.join(templatePath, 'vhost.yaml'), 'utf8');
+
+    // Template it
+    const vhostYaml = format(vhost, {
+        vhostuniq: randomstring.generate(12),
+        vhostservername: answers.vhostservername,
+        vhostdocroot: answers.vhostdocroot,
+        vhostport: answers.vhostport,
+        directoryuniq: randomstring.generate(12),
+    });
+
+    console.log(vhostYaml);
+
+    // Now write it to the right place
+    console.log(`Done!`);
+
+}).catch(function (error) {
+    console.log(`Something went wrong with the prompt.`);
+    console.log(error);
 });
